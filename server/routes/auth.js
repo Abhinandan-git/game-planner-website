57a7cbc6e9eecb084d93e8adac21f5e2a226f88f
Abhinandan Jain <nandanjain04@gmail.com>
2024-05-09 19:24:51 +0530

    tokenization of user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,26 +1,41 @@
 const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 const express = require('express');
-const router = express.Router()
+const bcrypt = require('bcryptjs');
+const router = express.Router();
 
-router.post('/', [
+const JWT_SECRET = 'abc';
+
+router.post('/createUser', [
 	body('username').trim().notEmpty().isLength({ min: 3 }),
 	body('password').trim().notEmpty().isLength({ min: 5 }),
-], (req, res) => {
+], async (req, res) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return res.status(400).json({ errors: errors.array() });
 	}
-	User.create({
-		username: req.body.username,
-		password: req.body.password
-	}).then(user => res.json(user)).catch(error => {
-		console.log(error);
-		res.json({
-			error: 'Enter unique value for username',
-			message: error.message
-		})
-	});
+	try {
+		let user = await User.findOne({ password: req.body.password });
+		if (user) {
+			return res.status(400).json({ error: "A user with this username already exists" })
+		}
+		const salt = await bcrypt.genSalt(10);
+		const securedPassword = await bcrypt.hash(req.body.password, salt);
+		user = await User.create({
+			username: req.body.username,
+			password: securedPassword
+		});
+		const data = {
+			user: {
+				id: user.id
+			}
+		}
+		const authToken = jwt.sign(data, JWT_SECRET);
+		res.json({ authtoken: authToken });
+	} catch (error) {
+		res.status(500).send("Error");
+	}
 });
 
 module.exports = router;
