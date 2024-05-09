ec1be32b3a08ef706f66c006ddc7b91ecf496108
Abhinandan Jain <nandanjain04@gmail.com>
2024-05-09 18:59:41 +0530

    User validation

--- server/db.js
+++ server/db.js
@@ -1,16 +1,10 @@
-const { MongoClient } = require('mongodb');
 const mongoose = require("mongoose");
 
 const mongoURI = "mongodb+srv://databaseAdmin:UO2OE8yrYQ5POVk5@gamedatabase.y9jviu5.mongodb.net/";
-const client = new MongoClient(mongoURI);
 
 const connectToMongo = async () => {
-	try {
-		await client.connect();
-		console.log('Connected to MongoDB Atlas');
-	} catch (error) {
-		console.error('Error connecting to MongoDB Atlas', error);
-	}
+	mongoose.connect(mongoURI);
+	console.log('Connected to MongoDB Atlas');
 }
 
 module.exports = connectToMongo;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,26 @@
+const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 const express = require('express');
 const router = express.Router()
 
-router.post('/', (req, res) => {
-	const user = User(req.body);
-	user.save();
-	res.send(req.body);
+router.post('/', [
+	body('username').trim().notEmpty().isLength({ min: 3 }),
+	body('password').trim().notEmpty().isLength({ min: 5 }),
+], (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+	User.create({
+		username: req.body.username,
+		password: req.body.password
+	}).then(user => res.json(user)).catch(error => {
+		console.log(error);
+		res.json({
+			error: 'Enter unique value for username',
+			message: error.message
+		})
+	});
 });
 
 module.exports = router;
