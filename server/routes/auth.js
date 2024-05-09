5eb410fe7bcac29bfea0974a5be7b4fc67a8c5c1
Abhinandan Jain <nandanjain04@gmail.com>
2024-05-09 21:28:22 +0530

    User data working

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,9 +4,11 @@ const jwt = require('jsonwebtoken');
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
+const fetchUser = require('../middleware/fetchUser');
 
 const JWT_SECRET = 'abc';
 
+// Create User Endpoint
 router.post('/createUser', [
 	body('username').trim().notEmpty().isLength({ min: 3 }),
 	body('password').trim().notEmpty().isLength({ min: 5 }),
@@ -34,7 +36,49 @@ router.post('/createUser', [
 		const authToken = jwt.sign(data, JWT_SECRET);
 		res.json({ authtoken: authToken });
 	} catch (error) {
-		res.status(500).send("Error");
+		res.status(500).send("Internal Server Error");
+	}
+});
+
+// Login Endpoint
+router.post('/login', [
+	body('username').trim(),
+	body('password').exists(),
+], async (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+	const { email, password } = req.body;
+	try {
+		let user = await User.findOne({ email });
+			if (!user) {
+				return res.status(400).json({ errors: "Login with correct credentials" });
+			}
+			const passwordCompare = await bcrypt.compare(password, user.password);
+			if (!passwordCompare) {
+				return res.status(400).json({ errors: "Login with correct credentials" });
+			}
+			const payload = {
+				user: {
+					id: user.id
+				}
+			}
+			const authToken = jwt.sign(payload, JWT_SECRET);
+			res.json({ authToken });
+	} catch (error) {
+		res.status(500).send("Internal Server Error");
+	}
+});
+
+// Get user details
+router.post('/getuser', fetchUser, async (req, res) => {
+	try {
+		let userId = req.user.id;
+		const user = await User.findById(userId).select("-password");	
+		res.send(user);
+	} catch (error) {
+		res.status(500).send("Internal Server Error");
 	}
 });
 
